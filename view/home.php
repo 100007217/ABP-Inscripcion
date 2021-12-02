ee2e0513ffab7cdc916a1deb75e50e49b2766d6f
blum <alfredoblumtorres@gmail.com>
2021-12-02 18:22:24 +0100

    Second round

--- view/home.php
+++ view/home.php
@@ -10,13 +10,22 @@
 </head>
 <body>
     <header>
-        <div class="" id="section1">
-            <a href="#section2">Down</a>
+        <div class="menu" id="section1">
+            <nav>
+                <ul>
+                    <li><a class="opcionesMenu" href="">¿Eres voluntario?</a></li>
+                    </li>
+                </ul>
+            </nav>
         </div>
+            <a href="#section2">Down</a>
     </header>
         <div class="rleft">
-
-        </div>
+            <center>
+                <img src="../media/ajuntament-barcelona-logo.png" alt="">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5987.206062901622!2d2.1772792693115237!3d41.38270864917479!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x23821e1275bfee57!2sAyuntamiento%20de%20Barcelona!5e0!3m2!1ses!2ses!4v1638462743217!5m2!1ses!2ses" width="280" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </center>
+            </div>
         <div class="row container" id="section2">
             <div class="title">
                 <h1>ABP</h1>
@@ -29,12 +38,12 @@
                 try {
                     $query = $pdo->prepare("SELECT id_eve, nombre_eve, img_eve, fecha_inicio_eve, fecha_fin_eve, num_max_pers_eve FROM tbl_eventos WHERE DATE(fecha_inicio_eve) >= DATE(NOW())");
                     $query->execute();
-                    $datas = $query->fetchAll(PDO::FETCH_ASSOC); //Tipp de extracción de datos
+                    $datas = $query->fetchAll(PDO::FETCH_ASSOC); //Tipo de extracción de datos
 
                     foreach ($datas as $row) {
                         echo "<div class='eventcards'>"; //Clase de la extracción de datos, en forma de cartas
                             echo "<h1>".$row['nombre_eve']."</h1>"; 
-                            echo "<img width='250' height='170' src = '../media/".$row['img_eve']."'>";
+                            echo "<img width='220' height='170' src = '../media/".$row['img_eve']."'>";
                             echo "<p>"."Evento: ".$row['fecha_inicio_eve']."</p>";
                             echo "<p>"."Fin del evento: ".$row['fecha_fin_eve']."</p>";
                             echo "<p>"."Cantidad máxima de voluntarios: ".$row['num_max_pers_eve']."</p>";
@@ -53,7 +62,6 @@
                     throw $th;
                 }
             ?>
-            </br></br>
             <div class="slider" id="section1">
                     <ul>
                         <li><img src="../media/background2.png" alt=""></a></li>
