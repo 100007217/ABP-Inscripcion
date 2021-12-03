affc5c94201dd3b1631c77f817b1c651ec240bba
blum <alfredoblumtorres@gmail.com>
2021-12-03 01:06:12 +0100

    html

--- view/home.php
+++ view/home.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../css/style.css"
-      rel="stylesheet" type="text/css">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
     <title>Home</title>
 </head>
 <body>
@@ -13,23 +12,28 @@
         <div class="menu" id="section1">
             <nav>
                 <ul>
+                    <li><a class="opcionesMenu" href="./home.php">Home</a></li>
                     <li><a class="opcionesMenu" href="">¿Eres voluntario?</a></li>
-                    </li>
                 </ul>
             </nav>
         </div>
-            <a href="#section2">Down</a>
+        <center>
+            <form action="#section2">
+                <input type="submit" value="Empecemos" id="submit">
+            </form>
+        </center>
     </header>
         <div class="rleft">
             <center>
                 <img src="../media/ajuntament-barcelona-logo.png" alt="">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5987.206062901622!2d2.1772792693115237!3d41.38270864917479!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x23821e1275bfee57!2sAyuntamiento%20de%20Barcelona!5e0!3m2!1ses!2ses!4v1638462743217!5m2!1ses!2ses" width="280" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                <p>Para obtener mucha más información de este tipo de proyectos o incluso ir a más de manera voluntaria, puedes acercarte a las ofinas centrales, en este caso nuestro ayuntamiento, en el que se te solucionarán todo tipo de dudas, y incluso fidelizarte con la asociación.</p>
             </center>
             </div>
         <div class="row container" id="section2">
             <div class="title">
                 <h1>ABP</h1>
-                <a href="#section1">Up</a>  
+                <p>Te damos la bienvenida a nuestra página oficial de eventos benéficos, la cual permite participar a los eventos que más encagen con tu perfil caricativo.</p>
             </div>
             <?php
                 require_once "../services/connection.php";        
@@ -62,6 +66,10 @@
                     throw $th;
                 }
             ?>
+            <center>
+                <p>Tal como podrás apreciar, estas son algunas de nuestras actividades, realizadas con mucha pasión y aprecio hacia nuestro objetivo, poder disfrutar de hacer deporte, divertirnos y además hacerlo de forma benéfica.</p>
+                <p>Aquí os dejamos unos de nuestros mejores momentos.</p>
+            </center>
             <div class="slider" id="section1">
                     <ul>
                         <li><img src="../media/background2.png" alt=""></a></li>
@@ -70,10 +78,37 @@
                         <li><img src="../media/background4.jpg" alt=""></li>
                     </ul>
             </div>
+            <center>
+                <form action="#section1">
+                    <input type="submit" value="Home" id="submit2">
+                </form>
+            </center>
         </div>
-    </div>
-<footer>
-
-</footer>
+    <footer class="row">
+        <div class="foottitle">
+            <h1>Encuéntranos!</h1>
+        </div>
+        <div class="footcol">
+            <h3>Creadores</h3>
+            <img src="../media/logoloveevents.png" alt="">
+            <p>Entrepans calents</p>
+            <p>Begudes</p>
+            <p>Cafés</p>
+        </div>
+        <div class="footcol">
+            <h3>Creadores</h3>
+            <img src="../media/logoloveevents.png" alt="">
+            <p>Entrepans calents</p>
+            <p>Begudes</p>
+            <p>Cafés</p>
+        </div>
+        <div class="footcol">
+            <h3>Creadores</h3>
+            <img src="../media/logoloveevents.png" alt="">
+            <p>Entrepans calents</p>
+            <p>Begudes</p>
+            <p>Cafés</p>
+        </div>
+    </footer>
 </body>
 </html>
